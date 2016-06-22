d222e43b91f3cc1016ddaf1beff2517c7be6ddb5
Sam Jacob <Sam.Jacob@synechron.com>
2016-06-22 21:52:53 +0530

    Cluster | Tran Clustering -v5

--- clust.r
+++ clust.r
@@ -16,8 +16,20 @@ cluster <- cutree(clust,k=3)
 cluster
 #Hierarchial Clustering
 
+clust_data <- cbind(tran,cluster)
+g <- ggplot(clust_data,aes(x=c(1:371),y=tran$BALANCE,color=factor(clust_data$cluster),group=clust_data$cluster)) + geom_point(size=1.5)
+g <- g + ggtitle("AML 2.0 : Tran Clusters") + xlab("Transactions") + ylab("Account Balance") 
+g <- g + scale_color_manual(values=c("orange","green2","purple"),name="Customer Profiling",labels=c("Low","Mid Range","High"))
+g
+ggsave("E:/R/AML_2/Images/Tran_Clusters.png")
 
-#k-means Clustering
+
+
+
+
+
+#k-means Clustering 
+#Ignore this part
 cluster <- kmeans(tran[,7],3)
 cluster$cluster
 table(cluster$cluster, tran$ACC_ID)

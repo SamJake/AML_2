b5b1c3499bcbeb29dc93b6f3e272fa0985ada5f1
Sam Jacob <Sam.Jacob@synechron.com>
2016-07-08 15:41:28 +0530

    Analyse clusters | Numbering clusters & mean | -v19

--- sort.r
+++ sort.r
@@ -7,3 +7,30 @@ for( l in 1:length(kyc_cluster_list))
   print(paste("Sorted",l))
   print(kyc_cluster_list[[l]])
 }
+
+
+xyz1 <- tran_cluster_list_agg
+for( l in 1:length(tran_cluster_list_agg))
+{
+  print(paste("Unsorted",l))
+  print(tran_cluster_list_agg[[l]])
+  tran_cluster_list_agg[[l]] <- tran_cluster_list_agg[[l]][order(tran_cluster_list_agg[[l]]$Group.1),]
+  print(paste("Sorted",l))
+  print(tran_cluster_list_agg[[l]])
+}
+
+tran_cluster_means <- data.frame(cluster=integer(0),means=numeric(0))
+for( n in 1:length(tran_cluster_list_agg))
+{
+  tran_cluster_means[n,1]  <- n
+  tran_cluster_means[n,2] <- sum(tran_cluster_list_agg[[n]]$x)/length(tran_cluster_list_agg[[n]]$x)
+}
+
+kyc_cluster_means <- data.frame(cluster=integer(0),means=numeric(0))
+for( n in 1:length(kyc_cluster_list))
+{
+  kyc_cluster_means[n,1]  <- n
+  kyc_cluster_means[n,2]  <- sum(kyc_cluster_list[[n]]$INCOME)/length(kyc_cluster_list[[n]]$INCOME)
+}
+
+kyc_cluster_means
--- tran.r
+++ tran.r
@@ -42,23 +42,23 @@ for(i in 1:k)
 abc <- c("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z")
 
 
-k <- 3
-for(i in 1:k)
-{
-  name1 <- abc[i]#a,b,c
-  name2 <- paste(name1,i,sep="")#a1,b2,c3
-  name3 <- paste(name2,i,sep="")#a11,b22,c33
-  name4 <- paste(name3,"_",sep="")#a11_,b22_,c33_
-  acc <- get(name1)[,7]
-  bal <- get(name1)[,3]
-  assign(name2,aggregate(acc,list(bal),mean))
-  assign(name3,aggregate(acc,list(bal),length))
-  l <- get(name2)$x
-  len <- data.frame(x2 = l)
-  assign(name4,cbind(get(name3),len))
-}
+# k <- 3
+# for(i in 1:k)
+# {
+#   name1 <- abc[i]#a,b,c
+#   name2 <- paste(name1,i,sep="")#a1,b2,c3
+#   name3 <- paste(name2,i,sep="")#a11,b22,c33
+#   name4 <- paste(name3,"_",sep="")#a11_,b22_,c33_
+#   acc <- get(name1)[,7]
+#   bal <- get(name1)[,3]
+#   assign(name2,aggregate(acc,list(bal),mean))
+#   assign(name3,aggregate(acc,list(bal),length))
+#   l <- get(name2)$x
+#   len <- data.frame(x2 = l)
+#   assign(name4,cbind(get(name3),len))
+# }
 
-#Replacement for above ten lines
+#Replacement for above ten lines -- Aggregation - count & means
 tran_cluster_list_agg <- list()
 for(m in 1:length(tran_cluster_list)) 
 {
